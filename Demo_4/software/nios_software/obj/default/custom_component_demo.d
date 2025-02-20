d59a2f8a5e4d17b4ec2782106cf443f8048beb6d
Andrew <ajjagecko@gmail.com>
2025-02-20 11:14:17 -0500

    Forgot to commit demo 4

---
+++ Demo_4/software/nios_software/obj/default/custom_component_demo.d
@@ -0,0 +1,17 @@
+obj/default/custom_component_demo.o: custom_component_demo.c system.h \
+ ../nios_software_bsp/linker.h \
+ ../nios_software_bsp//HAL/inc/sys/alt_irq.h \
+ ../nios_software_bsp//HAL/inc/nios2.h \
+ ../nios_software_bsp//HAL/inc/alt_types.h ../nios_software_bsp/system.h
+
+system.h:
+
+../nios_software_bsp/linker.h:
+
+../nios_software_bsp//HAL/inc/sys/alt_irq.h:
+
+../nios_software_bsp//HAL/inc/nios2.h:
+
+../nios_software_bsp//HAL/inc/alt_types.h:
+
+../nios_software_bsp/system.h:
