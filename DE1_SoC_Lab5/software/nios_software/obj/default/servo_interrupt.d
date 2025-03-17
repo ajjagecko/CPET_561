5b874fb53c02e3359561014d81a3c7842616c336
Andrew Akre (RIT Student) <aja9676@rit.edu>
2025-03-17 16:13:14 -0400

    Lab 5, need to debug interrupts

---
+++ DE1_SoC_Lab5/software/nios_software/obj/default/servo_interrupt.d
@@ -0,0 +1,25 @@
+obj/default/servo_interrupt.o: servo_interrupt.c \
+ ../nios_software_bsp//HAL/inc/io.h \
+ ../nios_software_bsp//HAL/inc/alt_types.h system.h \
+ ../nios_software_bsp/linker.h ../nios_software_bsp//HAL/inc/alt_types.h \
+ ../nios_software_bsp//HAL/inc/sys/alt_irq.h \
+ ../nios_software_bsp//HAL/inc/nios2.h ../nios_software_bsp/system.h \
+ ../nios_software_bsp//drivers/inc/altera_avalon_pio_regs.h
+
+../nios_software_bsp//HAL/inc/io.h:
+
+../nios_software_bsp//HAL/inc/alt_types.h:
+
+system.h:
+
+../nios_software_bsp/linker.h:
+
+../nios_software_bsp//HAL/inc/alt_types.h:
+
+../nios_software_bsp//HAL/inc/sys/alt_irq.h:
+
+../nios_software_bsp//HAL/inc/nios2.h:
+
+../nios_software_bsp/system.h:
+
+../nios_software_bsp//drivers/inc/altera_avalon_pio_regs.h:
